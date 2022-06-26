0251c0010aceb121fe5038269316b862aa75ca69
Daniel escobar <2247@holbertonschool.com>
2022-06-26 18:59:04 -0500

    Update PauseMenu.cs

--- 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -28,26 +28,31 @@ public class PauseMenu : MonoBehaviour
             }
         }
     }
+    // Pause the game
     public void Pause()
     {
         Time.timeScale = 0;
         Canvas.SetActive(true);
     }
+    // Resume the game
     public void Resume()
     {
         Canvas.SetActive(false);
         Time.timeScale = 1;
     }
+    // restart the game
     public void Restart()
     {
         var level = SceneManager.GetActiveScene().name;
         Time.timeScale = 1;
         SceneManager.LoadScene(level);
     }
+    // Go to main menu
     public void MainMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
+    // Options menu
     public void Options()
     {
         SceneManager.LoadScene("Options");   
